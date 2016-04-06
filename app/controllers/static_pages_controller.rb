e16e67cd3ef1d3da7523be6a70c0d87d1657fc99
Kavin <azncelebration@gmail.com>
2016-04-06 01:56:05 -0700

    Set up routes to different tabs

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,9 @@ class StaticPagesController < ApplicationController
   def home
   end
   def education
-
+  end
+  def work
+  end
+  def projects
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
   get 'static_pages/home'
-
+  get '/education' => 'static_pages#education'
+  get '/work' => 'static_pages#work'
+  get '/projects' => 'static_pages#projects'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
