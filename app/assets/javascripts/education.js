e1041f8d2fed30f7828702b25cd251a57a6db113
Kavin <azncelebration@gmail.com>
2016-04-09 19:29:21 -0700

    Refactored from reactjs to jquery and set up panels

---
+++ app/assets/javascripts/education.js
@@ -0,0 +1,8 @@
+/**
+ * Created by ksrithon on 4/9/16.
+ */
+$(document).ready(function(){
+    $(document.getElementById("showInfoBtn")).click(function(){
+        $(document.getElementById("info")).toggle(500);
+    });
+});
