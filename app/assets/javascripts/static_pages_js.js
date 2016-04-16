ef9138c7361eba493c19b6112703a42dca60a209
Kavin <azncelebration@gmail.com>
2016-04-16 04:14:48 -0700

    Set up project panel template, got tech button working but need to work on switching divs

--- app/assets/javascripts/static_pages_js.js
+++ app/assets/javascripts/static_pages_js.js
@@ -5,4 +5,12 @@ $(document).ready(function(){
     $(document.getElementsByClassName("showInfoBtn")).click(function () {
         $(this).next('div').slideToggle(200);
     });
+    $(document.getElementsByClassName("projectBtn")).click(function () {
+        $(this).text(function(i, text) {
+            return text === "Tech" ? "Info" : "Tech";
+        });
+        $(this).next('div').text(function(i, text) {
+            return text === "Tech" ? "Info" : "Tech";
+        });
+    });
 });
